f50f74d7c3e21f87ba157b3eb07fd170a3b3a110
juanuranidev <juanuranidev@gmail.com>
2023-09-03 03:15:51 -0300

    Updated lib barrel

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,2 +1,7 @@
+export { default as formatMillisecondsToSeconds } from "./formatMillisecondsToSeconds";
+export { default as getRandomCode } from "./getRandomCode";
+export { default as javaScriptCodes } from "./javaScriptCodes";
 export { default as landingCodes } from "./landingCodes";
+export * from "./notifications";
 export { default as renderCodeCharacter } from "./renderCodeCharacter";
+export { default as typeScriptCodes } from "./typeScriptCodes";
