285ae629425a1392683bd817d8333ee16a91d98e
juanuranidev <juanuranidev@gmail.com>
2023-09-03 03:15:46 -0300

    Added get random code function to lib

---
+++ lib/utils/getRandomCode.ts
@@ -0,0 +1,18 @@
+import LANGUAGES_NAMES from "lib/constants/languages";
+import javaScriptCodes from "./javaScriptCodes";
+
+const getRandomCode = (language: string) => {
+  let index;
+
+  switch (language) {
+    case LANGUAGES_NAMES.JAVASCRIPT:
+      index = Math.floor(Math.random() * (javaScriptCodes.length - 0 + 1)) + 0;
+      return javaScriptCodes[index];
+
+    case LANGUAGES_NAMES.TYPESCRIPT:
+      index = Math.floor(Math.random() * (javaScriptCodes.length - 0 + 1)) + 0;
+      return javaScriptCodes[index];
+  }
+};
+
+export default getRandomCode;
