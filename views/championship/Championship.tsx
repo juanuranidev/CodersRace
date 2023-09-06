03bdb08a55ba746860e853e3ff0544a8f320ab5e
juanuranidev <juanuranidev@gmail.com>
2023-09-05 23:46:33 -0300

    Working on championship view

--- views/championship/Championship.tsx
+++ views/championship/Championship.tsx
@@ -1,7 +1,8 @@
+import { Container } from "@mantine/core";
 import React from "react";
 
 type Props = {};
 
 export default function Championship({}: Props) {
-  return <div>Championship</div>;
+  return <Container bg="background-primary.0" size="full"></Container>;
 }
