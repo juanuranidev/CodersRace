bafa74f39036c01a0814fb7203a68df331809181
juanuranidev <juanuranidev@gmail.com>
2023-09-06 23:42:54 -0300

    Working on Table component

--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Table as TableComponent } from "@mantine/core";
 
 type Props = {};
 
-export default function Table({}: Props) {
-  return <div>Table</div>;
+export default function Table({ rows, heads }: any) {
+  return <TableComponent></TableComponent>;
 }
