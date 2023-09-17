1f4e2b80f55e1b564794cd82122dd6f038ddea01
juanuranidev <juanuranidev@gmail.com>
2023-09-16 23:55:13 -0300

    Added python language

--- lib/utils/getRandomCode.ts
+++ lib/utils/getRandomCode.ts
@@ -1,6 +1,9 @@
-import LANGUAGES_NAMES from "lib/constants/languages";
-import javaScriptCodes from "./javaScriptCodes";
-import typeScriptCodes from "./typeScriptCodes";
+import {
+  pythonCodes,
+  javaScriptCodes,
+  typeScriptCodes,
+  LANGUAGES_NAMES,
+} from "lib";
 
 const getRandomCode = (language: string) => {
   switch (language) {
@@ -8,12 +11,12 @@ const getRandomCode = (language: string) => {
       return javaScriptCodes[
         Math.floor(Math.random() * javaScriptCodes.length)
       ];
-
     case LANGUAGES_NAMES.TYPESCRIPT:
-      // return typeScriptCodes[
-      //   Math.floor(Math.random() * typeScriptCodes.length)
-      // ];
-      return typeScriptCodes[0];
+      return typeScriptCodes[
+        Math.floor(Math.random() * typeScriptCodes.length)
+      ];
+    case LANGUAGES_NAMES.PYTHON:
+      return pythonCodes[Math.floor(Math.random() * pythonCodes.length)];
   }
 };
 
--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -3,5 +3,6 @@ export { default as getRandomCode } from "./getRandomCode";
 export { default as javaScriptCodes } from "./javaScriptCodes";
 export { default as landingCodes } from "./landingCodes";
 export * from "./notifications";
+export { default as pythonCodes } from "./pythonCodes";
 export { default as renderCodeCharacter } from "./renderCodeCharacter";
 export { default as typeScriptCodes } from "./typeScriptCodes";
