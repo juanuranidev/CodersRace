18e6aa833e9568bb6eebbdc591d60c073a64d705
juanuranidev <juanuranidev@gmail.com>
2023-09-18 23:53:12 -0300

    Modified language card animation component

--- views/selectLanguage/components/languageCard/LanguageCard.tsx
+++ views/selectLanguage/components/languageCard/LanguageCard.tsx
@@ -22,58 +22,51 @@ export default function LanguageCard({ language }: Props) {
   const { classes } = useStyles();
   const { hovered, ref } = useHover();
 
-  const imageAnimations = {
+  const cardAnimations = {
     cardHovered: {
-      x: "1000%",
-      scale: [1, 1.25],
-      rotate: [0, 90, 180, 360],
+      scale: [1, 0.95, 1],
       transition: { duration: 0.25 },
     },
-    cardNoHovered: { x: 0 },
   };
 
-  const textAnimations = {
+  const imageAnimations = {
     cardHovered: {
-      x: "-20%",
-      opacity: [0, 0, 1],
-      scale: [1, 1, 1.25],
-      transition: { duration: 0.25 },
+      scale: [1, 1.1, 1],
+      transition: { duration: 0.3 },
     },
-    cardNoHovered: { opacity: 1 },
   };
 
   return (
-    <Link
-      href={language?.link}
-      style={{ textDecoration: "none", width: "100%" }}
+    <motion.div
+      variants={cardAnimations}
+      animate={hovered ? "cardHovered" : ""}
     >
-      <Card
-        p="xl"
-        py="3rem"
-        ref={ref}
-        radius="md"
-        bg="background-secondary.0"
-        className={classes.languageCard}
-      >
-        <Flex align="center" justify="center">
-          <motion.img
-            width={50}
-            height={50}
-            src={language.image}
-            alt="javascript logo"
-            variants={imageAnimations}
-            animate={hovered ? "cardHovered" : "cardNoHovered"}
-          />
-          <motion.div
-            variants={textAnimations}
-            animate={hovered ? "cardHovered" : "cardNoHovered"}
-          >
+      <Link href={language?.link} style={{ textDecoration: "none" }}>
+        <Card
+          p="xl"
+          w="100%"
+          py="3rem"
+          ref={ref}
+          radius="md"
+          bg="background-secondary.0"
+          className={classes.languageCard}
+        >
+          <Flex align="center" justify="center">
+            <motion.img
+              width={50}
+              height={50}
+              src={language.image}
+              alt="javascript logo"
+              variants={imageAnimations}
+              animate={hovered ? "cardHovered" : ""}
+            />
+
             <Text color="text-primary.0" fw={600} fz={30} ml="sm">
               {language.name}
             </Text>
-          </motion.div>
-        </Flex>
-      </Card>
-    </Link>
+          </Flex>
+        </Card>
+      </Link>
+    </motion.div>
   );
 }
