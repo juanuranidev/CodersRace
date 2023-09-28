89d440b0b7adc99ce8c14fbc7f65ed8401b7d06c
juanuranidev <juanuranidev@gmail.com>
2023-09-27 23:59:35 -0300

    Working on mobile modal component

--- components/mobileModal/MobileModal.tsx
+++ components/mobileModal/MobileModal.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Card, Text } from "@mantine/core";
 
 type Props = {};
 
 export default function MobileModal({}: Props) {
-  return <div>MobileModal</div>;
+  return (
+    <Card>
+      <Text>Wow</Text>
+    </Card>
+  );
 }
