4f4050f25a78ddd555c6979f2ef4456fa84f2324
juanuranidev <juanuranidev@gmail.com>
2023-10-02 22:37:06 -0300

    Fix production problems

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,6 @@
 export { default as ComingSoonCard } from "./comingSoonCard/ComingSoonCard";
 export { default as Header } from "./header/Header";
 export { default as PublicLayout } from "./layout/PublicLayout";
-export { default as MobileView } from "./mobileView/mobileView";
+// export { default as MobileView } from "./mobileView/mobileView";
 export { default as RenderCode } from "./renderCode/RenderCode";
 export { default as Table } from "./table/Table";
