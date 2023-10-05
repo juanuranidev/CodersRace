e289914772472201c861668ccba5beeb983292db
juanuranidev <juanuranidev@gmail.com>
2023-10-04 23:58:50 -0300

    Deleted languages codes

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,9 +1,6 @@
 export { default as formatMillisecondsToSeconds } from "./formatMillisecondsToSeconds";
 export { default as getRandomCode } from "./getRandomCode";
-export { default as javaScriptCodes } from "./javaScriptCodes";
 export { default as landingCodes } from "./landingCodes";
 export * from "./notifications";
-export { default as pythonCodes } from "./pythonCodes";
 export { default as renderCodeCharacter } from "./renderCodeCharacter";
 export { default as request } from "./request";
-export { default as typeScriptCodes } from "./typeScriptCodes";
