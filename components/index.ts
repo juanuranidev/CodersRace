916b3e64bdce8f26796d80e6aaf980eb63c0b3d2
juanuranidev <juanuranidev@gmail.com>
2023-10-13 23:20:21 -0300

    Added auth constants and updated barrels

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,9 @@
+export { default as AuthHandler } from "./authHandler/AuthHandler";
 export { default as ComingSoonCard } from "./comingSoonCard/ComingSoonCard";
 export { default as Header } from "./header/Header";
 export { default as PublicLayout } from "./layout/PublicLayout";
 export { default as Loader } from "./loader/Loader";
 export { default as MobileView } from "./mobileView/MobileView";
 export { default as RenderCode } from "./renderCode/RenderCode";
+export { default as ScreenLoader } from "./screenLoader/ScreenLoader";
 export { default as Table } from "./table/Table";
---
+++ lib/constants/auth.ts
@@ -0,0 +1,8 @@
+const AUTH = {
+  STATES: {
+    LOADING: "loading",
+    AUTHENTICATED: "authenticated",
+  },
+};
+
+export default AUTH;
