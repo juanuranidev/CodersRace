f0a1d4bce65427d0aa5e25ee926314f82ce9409a
juanuranidev <juanuranidev@gmail.com>
2023-10-30 23:53:05 -0300

    Updated Loader component

--- components/loader/Loader.tsx
+++ components/loader/Loader.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import { Box } from "@mantine/core";
 
 export default function Loader() {
-  return <Box className="loader" />;
+  return (
+    <Box className="loader-container">
+      <Box className="loader" />
+    </Box>
+  );
 }
