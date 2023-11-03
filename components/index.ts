355eda4aeace4013f32f73552ce11b7add307be5
juanuranidev <juanuranidev@gmail.com>
2023-11-02 23:56:49 -0300

    Trying a new folder structure

--- components/index.ts
+++ components/index.ts
@@ -1,8 +1,1 @@
-export { default as AuthHandler } from "./authHandler/AuthHandler";
-export { default as ComingSoonCard } from "./comingSoonCard/ComingSoonCard";
-export { default as Header } from "./header/Header";
-export { default as PublicLayout } from "./layout/PublicLayout";
-export { default as Loader } from "./loader/Loader";
-export { default as MobileView } from "./mobileView/MobileView";
-export { default as RenderCode } from "./renderCode/RenderCode";
-export { default as ScreenLoader } from "./screenLoader/ScreenLoader";
+export * from "./shared";
