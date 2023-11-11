1d01def7558c3562b9072cb43fb9e666f47de53b
juanuranidev <juanuranidev@gmail.com>
2023-11-10 23:54:12 -0300

    Working on Tooltip ui component

--- components/ui/Tooltip.jsx
+++ components/ui/Tooltip.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import { Tooltip as MantineTooltip } from "@mantine/core";
 
-export default function Tooltip() {
-  return <MantineTooltip>Tooltip</MantineTooltip>;
+export default function Tooltip({
+  label,
+  transition,
+  withArrow,
+  multiline,
+  w,
+  h,
+}) {
+  return (
+    <MantineTooltip
+      label={label}
+      transition={transition}
+      withArrow={withArrow}
+      multiline={multiline}
+      w={w}
+      h={h}
+    >
+      Tooltip
+    </MantineTooltip>
+  );
 }
