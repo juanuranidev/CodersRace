752a90a283757945d777c622e8c88a5a8edcb883
juanuranidev <juanuranidev@gmail.com>
2023-11-23 00:00:08 -0300

    Fix

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Card as MantineCard } from "@mantine/core";
 
-export default function Card({ p, w, radius, bg, ref, ...props, ...children }) {
+export default function Card({ p, w, radius, bg, ref, children, ...props }) {
   return (
     <MantineCard p={p} w={w} radius={radius} bg={bg} ref={ref} {...props}>
-      {...children}
+      {children}
     </MantineCard>
   );
 }
