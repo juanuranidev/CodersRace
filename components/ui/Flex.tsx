a2f7b0e045f26241e0160f47bd70de4642ec36bf
juanuranidev <juanuranidev@gmail.com>
2023-12-05 23:56:08 -0300

    Trying working with ui components

--- components/ui/Flex.tsx
+++ components/ui/Flex.tsx
@@ -11,16 +11,32 @@ export default function Flex({
   alignItems = "center",
   justifyContent = "flex-start",
 }: any) {
+  const handleFormatJustifyContent = (justifyContent: string) => {
+    switch (justifyContent) {
+      case "flex-start":
+        return "justify-start";
+      case "flex-end":
+        return "justify-end";
+      case "center":
+        return "justify-center";
+      case "space-between":
+        return "justify-between";
+    }
+  };
+
+  const handleFormatFlexDirection = () => {};
+
   return (
-    <MantineFlex
-      gap={gap}
-      h={heigh}
-      w={width}
-      align={alignItems}
-      direction={direction}
-      justify={justifyContent}
+    <div
+      className={`flex ${handleFormatJustifyContent(justifyContent)}`}
+      // gap={gap}
+      // h={heigh}
+      // w={width}
+      // align={alignItems}
+      // direction={direction}
+      // justify={justifyContent}
     >
       {children}
-    </MantineFlex>
+    </div>
   );
 }
