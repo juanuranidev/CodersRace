ad617d9656d60a4b70c3b326abda3414b8ca2e8b
juanuranidev <juanuranidev@gmail.com>
2023-12-06 23:56:37 -0300

    Added theme to nextui plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,43 @@ module.exports = {
     // "./app/**/*.{js,ts,jsx,tsx,mdx}",
     // "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     // "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./views/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          layout: {},
+          colors: {},
+        },
+        dark: {
+          layout: {},
+          colors: {
+            background: "#15141a",
+            foreground: "#f9f9f9",
+            primary: {
+              50: "#FC5D1B",
+              100: "#e35418",
+              200: "#ca4a16",
+              300: "#b04113",
+              400: "#973810",
+              500: "#7e2f0e",
+              600: "#65250b",
+              700: "#4c1c08",
+              800: "#321305",
+              900: "#fc5d1b",
+              DEFAULT: "#e35418",
+              foreground: "#f9f9f9",
+            },
+            // "text-primary": "#f9f9f9",
+          },
+        },
+      },
+    }),
+  ],
 };
