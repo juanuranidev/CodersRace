bdc8c8cf38dd496c6463073429c4ef8af8194f2f
juanuranidev <juanuranidev@gmail.com>
2023-12-09 01:42:51 -0300

    Working on public layout

--- components/shared/layout/PublicLayout.tsx
+++ components/shared/layout/PublicLayout.tsx
@@ -3,12 +3,9 @@ import { AppShell, Box } from "@mantine/core";
 
 export default function PublicLayout({ children }: any) {
   return (
-    <div
-      className="container max-w-full"
-      // className="container w-fit mx-[1px] max-w-5xl"
-      //  ="background-primary.0" header={<Header />}
-    >
-      {children}
+    <div className="bg-background">
+      <Header />
+      <div className="pt-6">{children}</div>
     </div>
   );
 }
